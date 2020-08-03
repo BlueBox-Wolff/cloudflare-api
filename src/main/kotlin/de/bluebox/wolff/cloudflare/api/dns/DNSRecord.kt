fdb2cc52dc3c7c756c9e41a61a1c863551bf4c9f
LinuxServer <jerome.wolff1207@gmail.com>
2020-08-03 18:31:35 +0200

    Update variable visibility

--- src/main/kotlin/de/bluebox/wolff/cloudflare/api/dns/DNSRecord.kt
+++ src/main/kotlin/de/bluebox/wolff/cloudflare/api/dns/DNSRecord.kt
@@ -28,10 +28,10 @@ package de.bluebox.wolff.cloudflare.api.dns
 import com.google.gson.JsonObject
 
 data class DNSRecord(
-  private val type: String,
-  private val name: String,
-  private val content: String,
-  private val ttl: Int = 1,
-  private val proxied: Boolean = false,
-  private val data: JsonObject
+  val type: String,
+  val name: String,
+  val content: String,
+  val ttl: Int = 1,
+  val proxied: Boolean = false,
+  val data: JsonObject
 )
